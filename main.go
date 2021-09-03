83e6102973c64b500ed283f60b0355941a67611c
Jan Lauber <jan.lauber@protonmail.ch>
2021-09-03 11:47:39 +0200

    added get byte stream and write it to a File
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/upload", app.basicAuth(app.uploadFile))
+	mux.HandleFunc("/upload", app.basicAuth(app.getDataStream))
 	// mux.HandleFunc("/upload", app.uploadFile)
 
 	srv := &http.Server{
@@ -72,42 +72,39 @@ func (app *application) basicAuth(next http.HandlerFunc) http.HandlerFunc {
 	})
 }
 
-func (app *application) uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("File Upload Endpoint Hit")
+func (app *application) getDataStream(w http.ResponseWriter, r *http.Request) {
 
-	// parse max 10MB
-	r.ParseMultipartForm(10 << 20)
-
-	// FormFile returns the first file for the given key `myFile`
-	// it also returns the FileHeader so we can get the Filename,
-	// the Header and the size of the file
-	file, handler, err := r.FormFile("myFile")
+	bodyBytes, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-	// Create a temporary file within our temp-images directory that follows
-	// a particular naming pattern
-	tempFile, err := ioutil.TempFile("upload", "upload-*.cxls")
+	err = writeToFile(bodyBytes)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	defer tempFile.Close()
-
-	// read all of the contents of our uploaded file into a
-	// byte array
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// write this byte array to our temporary file
-	tempFile.Write(fileBytes)
-	// return that we have successfully uploaded our file!
+	// Debugging Print Function
+	// fmt.Printf("%s", bodyBytes)
 	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func writeToFile(b []byte) error {
+
+	content := []byte(b)
+
+	tempFile, err := ioutil.TempFile("data", "data-*.cxml")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	if _, err = tempFile.Write(content); err != nil {
+		log.Println("Failed to write the file", err)
+	}
+	if err := tempFile.Close(); err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("File written: %+v\n", tempFile.Name())
+
+	return nil
 }
